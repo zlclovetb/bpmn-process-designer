91fd29b3316e4a483c23c7a1cf283cea1402bdcc
paradm <zlc_tb@163.com>
2022-01-17 16:07:18 +0800

    添加validate bpmn的逻辑,添加自定义规则

---
+++ plugin/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  configs: {
+    recommended: {
+      rules: {
+        'target-namespace': 'error'
+      }
+    },
+    all: {
+      rules: {
+        'target-namespace': 'warn',
+        'no-manual-task': 'warn',
+        'no-node-users': 'error',
+      }
+    }
+  }
+}
